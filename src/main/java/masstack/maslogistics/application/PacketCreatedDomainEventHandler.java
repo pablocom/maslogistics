9c52edc1b1ebc40716aa530ef5e33b2503d4c55e
pablocom <p.c.ramirez5150@gmail.com>
2020-12-04 13:15:17 +0100

    Changes domain event handler to an interface

--- src/main/java/masstack/maslogistics/application/PacketCreatedDomainEventHandler.java
+++ src/main/java/masstack/maslogistics/application/PacketCreatedDomainEventHandler.java
@@ -3,13 +3,16 @@ package masstack.maslogistics.application;
 import masstack.maslogistics.domain.sharedKernel.DomainEventHandler;
 import masstack.maslogistics.domain.events.PacketCreated;
 
-public class PacketCreatedDomainEventHandler extends DomainEventHandler<PacketCreated> {
+public class PacketCreatedDomainEventHandler implements DomainEventHandler<PacketCreated> {
     @Override
     public void handleEvent(PacketCreated domainEvent) {
         System.out.println("Received PacketCreated domain event");
         System.out.println("AggregateId: " + domainEvent.getAggregateId());
         System.out.println("EventId: " + domainEvent.getEventId());
         System.out.println();
+
+        // here we could publish the event to a event bus in the
+        // case the event should be listened by another microservice
     }
 
     @Override
--- src/main/java/masstack/maslogistics/domain/sharedKernel/DomainEventHandler.java
+++ src/main/java/masstack/maslogistics/domain/sharedKernel/DomainEventHandler.java
@@ -1,6 +1,6 @@
 package masstack.maslogistics.domain.sharedKernel;
 
-public abstract class DomainEventHandler<T extends DomainEvent> {
-    public abstract void handleEvent(T domainEvent);
-    public abstract Class<T> subscribedToEventType();
+public interface DomainEventHandler<T extends DomainEvent> {
+    void handleEvent(T domainEvent);
+    Class<T> subscribedToEventType();
 }
